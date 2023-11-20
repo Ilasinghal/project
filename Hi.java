7222cb3d855f75552e2f2c2bfdf7a5960c35164a
Ilasinghal <ilasinghal.1995@gmail.com>
2023-11-20 17:49:25 +0530

    second change

--- Hi.java
+++ Hi.java
@@ -2,6 +2,6 @@ class Hi{
     public static void main(String[] args){  
     System.out.println("why Hello world...let's say Hi to the world "); 
     System.out.println("changes done at remote ");
-      
+     System.out.println("changes done at local"); 
     }  
     }  
